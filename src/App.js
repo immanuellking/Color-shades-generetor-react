6f24e3e04cc8a2829c7c8906929d12dec79b0de6
immanuellking <emmaebubeegbosi@gmail.com>
2023-05-23 22:07:40 +0100

    last commit

--- src/App.js
+++ src/App.js
@@ -6,17 +6,17 @@ import Values from "values.js";
 function App() {
   const [color, setColor] = useState("");
   const [error, setError] = useState(false);
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(new Values("#feeee9").all(10));
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     try {
       let colors = new Values(color).all(10);
-      console.log(colors);
+      setList(colors);
     } catch (error) {
-      setError(true)
-      console.log(error)
+      setError(true);
+      console.log(error);
     }
   };
 
@@ -30,7 +30,7 @@ function App() {
             value={color}
             onChange={(e) => setColor(e.target.value)}
             placeholder="#f15025"
-            className={`${error && "error"}`}
+            className={`${error ? "error" : null}`}
           />
           <button className="btn" type="submit">
             Submit
@@ -38,7 +38,16 @@ function App() {
         </form>
       </section>
       <section className="colors">
-        <h4>List goes here</h4>
+        {list.map((color, index) => {
+          return (
+            <SingleColor
+              key={index}
+              {...color}
+              index={index}
+              hexColor={color.hex}
+            />
+          );
+        })}
       </section>
     </>
   );
